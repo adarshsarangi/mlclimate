514020dc3be6fe60ae5da2d23df14568568f5f40
Adarsh Sarangi <adarshsarangi2@gmail.com>
2023-08-19 20:48:43 +0530

    temperature predict

--- app.py
+++ app.py
@@ -22,22 +22,49 @@ def weather_predict():
     freq = request.form.get('frequency')[0].lower()
     periods = int(request.form.get('period'))
     what = request.form.get('what')
-    m = load_precipitation()
     if what == 'temperature' : 
-        pass
+        m_max = load_temp_max()
+        m_min = load_temp_min()
+        df_max_temp = prediction(m_max, periods, freq)
+        df_min_temp = prediction(m_min, periods, freq)
+        df_max_temp.rename(columns={'yhat':'max'},inplace = True)
+        df_min_temp.rename(columns={'yhat':'min'},inplace = True)
+        df_min_temp.drop(['ds'], axis= 1,inplace = True)
+        df = pd.concat([df_max_temp, df_min_temp],axis= 1, join = 'inner')
+        json_data = convert_to_json(df)        
+        return render_template('prediction_temp.html',json_data=json_data)
+    
     elif what == 'wind' :
         m = load_wind()
+        df = prediction(m,periods, freq)
+        json_data = convert_to_json(df)
+        return render_template('prediction.html',json_data= json_data)
+    
     elif what == 'weather' :
-        pass
+        m = load_wind()
+        df = prediction(m,periods, freq)
+        json_data = convert_to_json(df)
+        return render_template('prediction.html',json_data=json_data)
+    
+    m = load_precipitation()
+    df= prediction(m,periods, freq)
+    json_data = convert_to_json(df)
+    return render_template('prediction.html', json_data= json_data)
+
+def convert_to_json(df):
+    json_data = df.to_json(orient = 'records')
+    json_data = json.loads(json_data)
+    return json_data
+
+
+def prediction(m, periods, freq):
     future_dataframe = m.make_future_dataframe(freq=freq, periods=periods)
     predictions = m.predict(future_dataframe)
     df = pd.DataFrame(predictions,columns = ['ds','yhat'])
     df['ds'] = df['ds'].dt.strftime('%Y-%m-%d')
     df['yhat'] = round(df['yhat'], 3)
     df = df[1460:1460+int(periods)]
-    json_data = df.to_json(orient='records')
-    json_data = json.loads(json_data)
-    return render_template('prediction.html',json_data=json_data)
+    return df
 
 def load_precipitation():
     with open('model.json','r') as fin : 
@@ -54,15 +81,10 @@ def load_temp_min() :
         m = model_from_json(fin.read())
     return m
 
-def load_temp(freq, periods) : 
-    max_model = load_temp_max()
-    min_model = load_temp_min()
-    
 
-def load_wind(freq, periods):
-    with open('model_temp_mid.json', 'r') as fin : 
+def load_wind():
+    with open('model_wind.json', 'r') as fin : 
         m = model_from_json(fin.read())
-    
     return m
 
 @app.route('/feed')
